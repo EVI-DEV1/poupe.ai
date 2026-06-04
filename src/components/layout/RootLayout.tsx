7ef1cee098793d89c5da686b5d951fdf67b78b9f
EVI-DEV1 <eviofc4@gmail.com>
2026-06-04 17:05:50 -0300

    atualizando

---
+++ src/components/layout/RootLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from '../shared/Header';
+export function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
